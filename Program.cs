345d4e0933030c1ad017c7e361be7fc419f75839
carlos <carlosacgr@gmail.com>
2020-11-26 15:05:27 -0300

    sistema de produtos iniciado

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Sistema_de_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variáveis
+            string[] nomes = new string[10];
+            float[] preco = new float[10];
+            bool promocao;
+
+
+
+
+
+
+
+            
+            //funções
+            string CadastrarProduto(string nome,float preco,bool promocao){
+                
+            }
+
+            string ListarProdutos(){
+
+            }
+
+            string MostrarMenu(){
+
+            }
+
+
+        }
+    }
+}
+
+// Nesta aula vamos aplicar o seguinte projeto para gerenciamento de 10 produtos pelo console:
+
+// Os produtos terão os seguintes atributos:
+
+// - string Nome
+// - float Preco
+// - bool Promocao (se está em promoção ou não)
+
+// O sistema deverá ter as seguintes funçoes:
+
+// - CadastrarProduto
+// - ListarProdutos
+// - MostrarMenu
+
+//Algoritmo
+//entrada ==>> nome do produto + preço do produto + informação se está em promoção ou não
+//processamento ==>> 
+//saída ==>> 
